51e07868aa4bec69fac2cc187c8fe983d4fa9d29
tatyler <tatyler1@asu.edu>
2023-03-26 22:22:48 -0400

    Added comments to tests, added more tests

--- src/test/java/BlackBoxGiven.java
+++ src/test/java/BlackBoxGiven.java
@@ -45,6 +45,12 @@ public class BlackBoxGiven {
 
 
     // normal experience when healthy
+
+    /** dealDamage()
+     * Boundry Value Analysis/ Equivalence Partition
+     * Health = 100
+     * 5 Experience gained, Normal damage
+     */
     @Test
     public void dealtDamageNormalExperience() {
         Wizard wiz = new Wizard();
@@ -76,7 +82,11 @@ public class BlackBoxGiven {
 
     //Tests for takeDamage
     @Test
-    //Equivalence Partition
+    /** takeDamage()
+     * Equivalence Partition
+     * 0 > BlowDamage > 100
+     * Represenative: 5
+     */
     public void takeDamageNormalExperience() {
         Wizard wiz = new Wizard();
 
@@ -84,6 +94,11 @@ public class BlackBoxGiven {
         game.takeDamage(wiz, 5);
         assertEquals(wiz.health, 96);
     }
+    /** takeDamage()
+     * Equivalence Partition / Boundry Value Analysis
+     * BlowDamage > 100
+     * Represenative: 101
+     */
     @Test
     public void takeDamageOneLargeHit() {
         Wizard wiz = new Wizard();
@@ -92,7 +107,11 @@ public class BlackBoxGiven {
         game.takeDamage(wiz, 101);
         assertEquals(wiz.health, 0);
     }
-
+    /** takeDamage()
+     * Equivalence Partition / Boundry Value Analysis
+     * BlowDamage < 0
+     * Represenative: -1
+     */
     @Test
     public void takeDamageNegative() {
         Wizard wiz = new Wizard();
@@ -102,6 +121,11 @@ public class BlackBoxGiven {
         assertEquals(wiz.health, 101);
     }
 
+    /** takeDamage()
+     * Equivalence Partition / Boundry Value Analysis
+     * BlowDamage == protection
+     * Represenative: 1
+     */
     @Test
     public void takeDamageEqualProtection() {
         Wizard wiz = new Wizard();
@@ -111,6 +135,11 @@ public class BlackBoxGiven {
         assertEquals(wiz.health, 100);
         assertEquals(wiz.experience, 0);
     }
+    /** takeDamage()
+     * Equivalence Partition / Boundry Value Analysis
+     * BlowDamage == 0
+     * Represenative: 0
+     */
     @Test
     public void takeDamageNoDamage() {
         Wizard wiz = new Wizard();
@@ -119,14 +148,11 @@ public class BlackBoxGiven {
         game.takeDamage(wiz, 0);
         assertEquals(wiz.health, 100);
     }
-    //Boundry Value Analysis for takeDamage()
-    @Test
-    public void takeDamageNegativeDamage() {
-        Wizard wiz = new Wizard();
-
-        game.takeDamage(wiz, -1);
-        assertEquals(wiz.health, 101);
-    }
+    /** takeDamage()
+     * Boundry Value Analysis
+     * BlowDamage = 2
+     * 1 Health should be lost
+     */
     @Test
     public void takeDamageTwoDamage() {
         Wizard wiz = new Wizard();
@@ -134,6 +160,11 @@ public class BlackBoxGiven {
         game.takeDamage(wiz, 2);
         assertEquals(wiz.health, 99);
     }
+    /** takeDamage()
+     * Boundry Value Analysis
+     * BlowDamage = 24
+     * 23 Health should be lost
+     */
     @Test
     public void takeDamage24Damage() {
         Wizard wiz = new Wizard();
@@ -141,6 +172,11 @@ public class BlackBoxGiven {
         game.takeDamage(wiz, 24);
         assertEquals(wiz.health, 77);
     }
+    /** takeDamage()
+     * Boundry Value Analysis
+     * BlowDamage = 10
+     * 9 Health should be lost
+     */
     @Test
     public void takeDamage10Damage() {
         Wizard wiz = new Wizard();
@@ -148,6 +184,11 @@ public class BlackBoxGiven {
         game.takeDamage(wiz, 10);
         assertEquals(wiz.health, 91);
     }
+    /** takeDamage()
+     * Boundry Value Analysis
+     * BlowDamage = 99
+     * 98 Health should be lost
+     */
     @Test
     public void takeDamage99Damage() {
         Wizard wiz = new Wizard();
@@ -155,6 +196,11 @@ public class BlackBoxGiven {
         game.takeDamage(wiz, 99);
         assertEquals(wiz.health, 2);
     }
+    /** takeDamage()
+     * Boundry Value Analysis
+     * BlowDamage = 100
+     * 99 Health should be lost
+     */
     @Test
     public void takeDamage100Damage() {
         Wizard wiz = new Wizard();
@@ -166,6 +212,11 @@ public class BlackBoxGiven {
 
 
     //dealdamage() Tests
+    /** dealdamage()
+     * Equivalence Partition
+     * Wiz.health < 0
+     * Representative : -5
+     */
     @Test
     public void dealDamageNegativeHealth() {
         Wizard wiz = new Wizard();
@@ -174,6 +225,11 @@ public class BlackBoxGiven {
         game.dealDamage(wiz);
         assertEquals(wiz.experience, 0);
     }
+    /** dealDamage()
+     * Equivalence Partition/ Boundry Value Analysis
+     * Wiz.health == 0
+     * Representative : 0
+     */
     @Test
     public void dealDamageNoHealth() {
         Wizard wiz = new Wizard();
@@ -182,6 +238,11 @@ public class BlackBoxGiven {
         game.dealDamage(wiz);
         assertEquals(wiz.experience, 0);
     }
+    /** dealDamage()
+     * Equivalence Partition
+     * 0 >Wiz.health > 10
+     * Representative : 6
+     */
     @Test
     public void dealDamageDoubleDamage() {
         Wizard wiz = new Wizard();
@@ -191,6 +252,11 @@ public class BlackBoxGiven {
         assertEquals(wiz.experience, 5);
     }
     //dealdamage() Boundry Value Analysis
+    /** dealdamage()
+     * Boundry Value Analysis
+     * Health = 1
+     * 5 Experience gained, Double damage
+     */
     @Test
     public void dealDamage1Health() {
         Wizard wiz = new Wizard();
@@ -199,6 +265,11 @@ public class BlackBoxGiven {
         assertEquals(game.dealDamage(wiz),10);
         assertEquals(wiz.experience, 5);
     }
+    /** dealDamage()
+     * Boundry Value Analysis
+     * Health = 10
+     * 5 Experience gained, Normal damage
+     */
     @Test
     public void dealDamage10Health() {
         Wizard wiz = new Wizard();
@@ -207,6 +278,12 @@ public class BlackBoxGiven {
         assertEquals(game.dealDamage(wiz),5);
         assertEquals(wiz.experience, 5);
     }
+
+    /** dealDamage()
+     * Boundry Value Analysis
+     * Health = 25
+     * 5 Experience gained, Normal damage
+     */
     @Test
     public void dealDamage25Health() {
         Wizard wiz = new Wizard();
@@ -216,6 +293,12 @@ public class BlackBoxGiven {
         assertEquals(i,5);
         assertEquals(wiz.experience, 5);
     }
+
+    /** dealDamage()
+     * Boundry Value Analysis
+     * Health = 50
+     * 5 Experience gained, Normal damage
+     */
     @Test
     public void dealDamage50Health() {
         Wizard wiz = new Wizard();
@@ -225,6 +308,12 @@ public class BlackBoxGiven {
         assertEquals(i,5);
         assertEquals(wiz.experience, 5);
     }
+
+    /** dealDamage()
+     * Boundry Value Analysis
+     * Health = 99
+     * 5 Experience gained, Normal damage
+     */
     @Test
     public void dealDamage99Health() {
         Wizard wiz = new Wizard();
@@ -236,6 +325,12 @@ public class BlackBoxGiven {
     }
 
     //attack(wiz,bar) tests
+
+    /** attack()
+     * Equivalence Partition
+     * Wiz.health < 0, bar.health < 0
+     * -5, -5
+     */
     @Test
     public void attackNegativeHealth() {
         Wizard wiz = new Wizard();
@@ -248,6 +343,11 @@ public class BlackBoxGiven {
         assertEquals(wiz.health,-5);
         assertEquals(bar.health,-5);
     }
+    /** attack()
+     * Equivalence Partition/ Boundry Value Analysis
+     * Wiz.health ==0, bar.health == 0
+     * 0, 0
+     */
     @Test
     public void attackBothZero() {
         Wizard wiz = new Wizard();
@@ -260,6 +360,12 @@ public class BlackBoxGiven {
         assertEquals(wiz.health,0);
         assertEquals(bar.health,0);
     }
+    /** attack()
+     * Equivalence Partition/ Boundry Value Analysis
+     * Wiz.health > 0, bar.health > 0
+     * 100, 100
+     *
+     */
     @Test
     public void attackNormalExperience() {
         Wizard wiz = new Wizard();
@@ -269,6 +375,11 @@ public class BlackBoxGiven {
         assertEquals(wiz.health,91);
         assertEquals(bar.health,102);
     }
+    /** attack()
+     * Equivalence Partition
+     * 0 > Wiz.health > 10,  0 > bar.health > 10
+     * 8,8
+     */
     @Test
     public void attackDoubleDamage() {
         Wizard wiz = new Wizard();
@@ -280,6 +391,11 @@ public class BlackBoxGiven {
         assertEquals(wiz.health,0);
         assertEquals(bar.health,8);
     }
+    /** attack()
+     * Equivalence Partition
+     * Wiz.health < 0 , bar.health > 0
+     * -5, 100
+     */
     @Test
     public void attackOneNegative() {
         Wizard wiz = new Wizard();
@@ -290,4 +406,53 @@ public class BlackBoxGiven {
         assertEquals(wiz.health,-5);
         assertEquals(bar.health,100);
     }
+    /** attack()
+     * Boundry Value Analysis
+     * Wiz.health = 1 , bar.health = 1
+     * wiz dies, bar one health
+     */
+    @Test
+    public void attackOneHealth() {
+        Wizard wiz = new Wizard();
+        Barbarian bar = new Barbarian();
+
+        wiz.health = 1;
+        bar.health = 1;
+        game.attack(wiz,bar);
+        assertEquals(wiz.health,0);
+        assertEquals(bar.health,1);
+    }
+    /** attack()
+     * Boundry Value Analysis
+     * Wiz.health = 50 , bar.health = 50
+     * wiz 41 health, bar 52 health
+     */
+    @Test
+    public void attack50Health() {
+        Wizard wiz = new Wizard();
+        Barbarian bar = new Barbarian();
+
+        wiz.health = 50;
+        bar.health = 50;
+        game.attack(wiz,bar);
+        assertEquals(wiz.health,41);
+        assertEquals(bar.health,52);
+    }
+    /** attack()
+     * Boundry Value Analysis
+     * Wiz.health = 99 , bar.health = 99
+     * wiz 90 health, bar 101 health
+     */
+    @Test
+    public void attack99Health() {
+        Wizard wiz = new Wizard();
+        Barbarian bar = new Barbarian();
+
+        wiz.health = 99;
+        bar.health = 99;
+        game.attack(wiz,bar);
+        assertEquals(wiz.health,90);
+        assertEquals(bar.health,101);
+    }
+
 }
