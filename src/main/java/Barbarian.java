dfa62397e1d4d384ccea82b7f6e5cc9e51772609
tracy <tatyler1@asu.edu>
2023-04-02 20:39:27 -0400

    Defect-6 Fixed other classes to setters

--- src/main/java/Barbarian.java
+++ src/main/java/Barbarian.java
@@ -1,9 +1,9 @@
 public class Barbarian extends Character {
 
     public Barbarian() {
-        protection = 10;
-        damage = 10;
-        speed = 2.0;
+        setProtection(10);
+        setDamage(10);
+        setSpeed(2.0);
         pointsPerLevel = 20;
     }
 
