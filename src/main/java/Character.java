b9c8628f30f5c41408413241ce772be75e56c076
tracy <tatyler1@asu.edu>
2023-04-02 20:17:36 -0400

    Setters/Getters added

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -18,4 +18,60 @@ public class Character {
         System.out.println("Points per Level: " + pointsPerLevel);
         System.out.println("\n");
     }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setPointsPerLevel(int pointsPerLevel) {
+        this.pointsPerLevel = pointsPerLevel;
+    }
+
+    public void setProtection(int protection) {
+        this.protection = protection;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getPointsPerLevel() {
+        return pointsPerLevel;
+    }
+
+    public int getProtection() {
+        return protection;
+    }
 }
