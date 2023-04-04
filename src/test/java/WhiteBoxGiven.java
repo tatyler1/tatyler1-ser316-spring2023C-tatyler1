0cae5bf7a6a77ee2134998fd536a8d9a90bfe54c
Tracy Tyler <tatyler1@asu.edu>
2023-04-04 00:18:53 -0400

    WhiteBoxGiven updated

--- src/test/java/WhiteBoxGiven.java
+++ src/test/java/WhiteBoxGiven.java
@@ -27,5 +27,51 @@ public class WhiteBoxGiven {
         assertEquals(wiz1.experience, 7);
         assertEquals(wiz2.experience, 7);
     }
+    @Test
+    public void emptyGameplayArray() {
+        GamePlay game = new GamePlay(new Barbarian(), null);
+        int i = 0;
+        for(Character opponent : game.Opponents) {
+            System.out.println(opponent);
+            i++;
+        }
+        assertEquals(1,i);
+    }
+    //Play should not calculate experience since it is already done in attack()
+    @Test
+    public void playSpeedEquals0() {
+        Character wiz1 = new Wizard();
+        Character wiz2 = new Wizard();
+        wiz1.speed = 0;
+        wiz2.speed = 0;
+
+        GamePlay game = new GamePlay(wiz1,wiz2);
+
+        game.play();
+
+        assertEquals(7,wiz1.experience);
+
+    }
+
+    @Test
+    public void opponentDead() {
+        Character wiz1 = new Wizard();
+        Character wiz2 = new Wizard();
+        wiz2.health = 0;
+
+        GamePlay game = new GamePlay(wiz1,wiz2);
+        game.play();
+        assertEquals(game.Opponents.size(), 0);
+    }
+    @Test
+    public void opponentVeryDead() {
+        Character wiz1 = new Wizard();
+        Character wiz2 = new Wizard();
+        wiz2.health = -100;
+
+        GamePlay game = new GamePlay(wiz1,wiz2);
+        game.play();
+        assertEquals(game.Opponents.size(), 0);
+    }
 
 }
